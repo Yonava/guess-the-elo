8009b69d1ecf302f75f598522b69fc76a3164ec8
yonava <yona@vossandreae.com>
2024-10-14 10:39:33 -0400

    added sheets api classes

---
+++ src/constants.ts
@@ -0,0 +1,66 @@
+/**
+ * Constants for the server
+ * @module constants
+ * @category Server
+*/
+
+import { google } from 'googleapis';
+
+const localUri = 'http://localhost:3000/auth';
+const prodUri = '';
+
+/**
+ * @type {string}
+ * @description the redirect URI for the google OAuth process as defined on the google cloud console
+ */
+const redirectUri = process.env.NODE_ENV ? prodUri : localUri;
+
+/**
+ * @type {Object}
+ * @description enumerable error states encountered during the auth process
+*/
+const AUTH_ERRORS = {
+  // when the client token is invalid or empty
+  INVALID_CLIENT_TOKEN: 'INVALID_CLIENT_TOKEN',
+  // when the client token is expired, and a new one has been issued in the response
+  NEW_CLIENT_TOKEN_ISSUED: 'NEW_CLIENT_TOKEN_ISSUED',
+  // when the google oauth code provided by the client is invalid
+  INVALID_GOOGLE_OAUTH_CODE: 'INVALID_GOOGLE_OAUTH_CODE',
+  // when the google oauth refresh token is invalid
+  INVALID_GOOGLE_OAUTH_REFRESH_TOKEN: 'INVALID_GOOGLE_OAUTH_REFRESH_TOKEN',
+  // when the google oauth access token is invalid
+  INVALID_GOOGLE_OAUTH_ACCESS_TOKEN: 'INVALID_GOOGLE_OAUTH_ACCESS_TOKEN',
+  // when google oauth url fails to generate
+  GOOGLE_OAUTH_URL_GENERATION_FAILED: 'GOOGLE_OAUTH_URL_GENERATION_FAILED',
+} as const;
+
+/**
+ * @type {string[]}
+ * @description the google OAuth scopes required for the application
+ */
+const googleOAuthScope = [
+  'https://www.googleapis.com/auth/spreadsheets'
+]
+
+/**
+ * @type {Object}
+ * @description the spreadsheet IDs as defined by google sheets for which the GoogleSheet class interfaces
+*/
+const spreadsheetIds = {
+  dev: '1gKTFBN9lITCEyPKDWG31_yya-x3R3IRe6VH3Qa3sk1A',
+  production: '',
+}
+
+const authClient = () => new google.auth.OAuth2(
+  process.env.GOOGLE_OAUTH_CLIENT_ID,
+  process.env.GOOGLE_OAUTH_CLIENT_SECRET,
+  redirectUri
+)
+
+export {
+  AUTH_ERRORS,
+  redirectUri,
+  googleOAuthScope,
+  spreadsheetIds,
+  authClient
+}
---
+++ src/sheets.ts
@@ -0,0 +1,104 @@
+import { google } from 'googleapis';
+import { authClient, spreadsheetIds, AUTH_ERRORS } from './constants';
+
+export default class GoogleSheet {
+  spreadsheetId;
+  sheets;
+
+  writable(range: string, data: string[][]) {
+    return {
+      spreadsheetId: this.spreadsheetId,
+      range,
+      valueInputOption: 'RAW',
+      resource: {
+        values: data
+      }
+    } as const;
+  }
+
+  constructor(accessToken: string) {
+    const { dev: devSheetId, production: prodSheetId } = spreadsheetIds;
+    this.spreadsheetId = process.env.NODE_ENV === 'production' ? prodSheetId : devSheetId;
+    const auth = authClient();
+    try {
+      auth.setCredentials({
+        access_token: accessToken
+      });
+
+      this.sheets = google.sheets({
+        version: 'v4',
+        auth,
+      });
+    } catch (err) {
+      throw AUTH_ERRORS.INVALID_GOOGLE_OAUTH_ACCESS_TOKEN;
+    }
+  }
+
+  async getRange(range: string) {
+    const response = await this.sheets.spreadsheets.values.get({
+      spreadsheetId: this.spreadsheetId,
+      range,
+    });
+
+    return response.data.values;
+  }
+
+  async getRanges(ranges: string[]) {
+    const response = await this.sheets.spreadsheets.values.batchGet({
+      spreadsheetId: this.spreadsheetId,
+      ranges,
+    });
+
+    return response.data.valueRanges;
+  }
+
+  async deleteByRow(range: string, row: number) {
+    await this.sheets.spreadsheets.values.clear({
+      spreadsheetId: this.spreadsheetId,
+      range: `${range}!A${row}:Z${row}`,
+    });
+  }
+
+  async updateByRow(range: string, row: number, data: string[][]) {
+    await this.sheets.spreadsheets.values.update(
+      this.writable(`${range}!A${row}:Z${row}`, data)
+    );
+  }
+
+  async postInRange(range: string, data: string[][]) {
+    const rangeData = await this.getRange(range);
+
+    if (!rangeData) {
+      throw new Error('Range not found');
+    }
+
+    if (data.length > 1) {
+      await this.sheets.spreadsheets.values.update(
+        this.writable(range, [
+          ...rangeData,
+          ...data,
+        ])
+      );
+      return rangeData.length + 1;
+    }
+
+    let insertRow = rangeData.findIndex(row => row.join('') === '');
+    insertRow = insertRow === -1 ? rangeData.length + 1 : insertRow + 1;
+
+    await this.sheets.spreadsheets.values.update(
+      this.writable(`${range}!A${insertRow}:Z${insertRow}`, data)
+    );
+
+    return insertRow;
+  }
+
+  async replaceRange(range: string, data: string[][]) {
+    await this.sheets.spreadsheets.values.clear({
+      spreadsheetId: this.spreadsheetId,
+      range,
+    });
+
+    const payload = this.writable(range, data);
+    await this.sheets.spreadsheets.values.update(payload);
+  }
+}
